10072bc6808b62377110259c29e61ceb4fbcded5
Younghak Lee <younghl@nexon.co.kr>
2021-10-06 17:50:49 +0900

    refactor(Viewer): Viewer Refactoring
    String.format 에서 System.out.printf로 refactoring

--- src/main/java/baseball/Viewer.java
+++ src/main/java/baseball/Viewer.java
@@ -6,22 +6,26 @@ public class Viewer {
 	}
 
 	public static void printComplete(int num) {
-		System.out.println(String.format("%d개의 숫자를 모두 맞히셨습니다! 게임 끝", num));
+		System.out.printf("%d개의 숫자를 모두 맞히셨습니다! 게임 끝%n", num);
 		System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
 	}
 
 	public static void printResult(int[] result) {
 		if (result[0] > 0) {
-			System.out.print(String.format("%d스트라이크 ", result[0]));
+			System.out.printf("%d스트라이크 ", result[0]);
 		}
 
 		if (result[1] > 0) {
-			System.out.print(String.format("%d볼", result[1]));
+			System.out.printf("%d볼", result[1]);
 		}
 
 		if (result[0] == 0 && result[1] == 0) {
 			System.out.print("낫싱");
 		}
 		System.out.println();
+	}
+
+	public static void printError(String msg) {
+		System.out.printf("[ERROR] %s%n", msg);
 	}
 }
